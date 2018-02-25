02c637027e40c1426c84db98bd0d52cdd4ae2e00
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-02-25 15:07:33 -0800

    More ABC api.

--- src/utilities/Server.js
+++ src/utilities/Server.js
@@ -253,7 +253,7 @@ class Server {
     server.post('/getaccount', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send(JSON.stringify('doc test'));
+      res.send('');
     });
 
     server.post('/getaddednodeinfo', (req, res) => {
@@ -275,18 +275,17 @@ class Server {
 
     server.post('/getaddressesbyaccount', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
-
-      res.send(JSON.stringify([
-        "mjSk1Ny9spzU2fouzYgLqGUD8U41iR35QN",
-        "mft61jjkmiEJwJ7Zw3r1h344D6aL1xwhma",
-        "mmXgiR6KAhZCyQ8ndr2BCfEq1wNG2UnyG6"
-      ]));
+      let addresses = [];
+      store.get('addresses').forEach((address, index) => {
+        addresses.push(BitcoinCash.toCashAddress(BitcoinCash.fromWIF(address.privateKeyWIF).getAddress()));
+      });
+      res.send(addresses);
     });
 
     server.post('/getbalance', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send(JSON.stringify('1.99900000'));
+      res.send(JSON.stringify('0.00000000'));
     });
 
     server.post('/getbestblockhash', (req, res) => {
@@ -298,27 +297,35 @@ class Server {
     server.post('/getblock', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send(JSON.stringify({
-      	"hash": "00000000839a8e6886ab5951d76f411475428afc90947ee320161bbf18eb6048",
-      	"confirmations": 447014,
-      	"strippedsize": 215,
-      	"size": 215,
-      	"weight": 860,
-      	"height": 1,
-      	"version": 1,
-      	"versionHex": "00000001",
-      	"merkleroot": "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098",
-      	"tx": [
-      		"0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098"
-      	],
-      	"time": 1231469665,
-      	"mediantime": 1231469665,
-      	"nonce": 2573394689,
-      	"bits": "1d00ffff",
-      	"difficulty": 1,
-      	"chainwork": "0000000000000000000000000000000000000000000000000000000200020002",
-      	"previousblockhash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-      	"nextblockhash": "000000006a625f06636b8bb6ac7b960a8d03705d1ace08b1a19da3fdcc99ddbd"
+      res.send(JSON.stringify({ hash: '000000000000000002eafe2494f7dd1f678988217a12e8ab20d3594affd03392',
+        confirmations: 1,
+        size: 3827,
+        height: 518990,
+        version: 536870912,
+        versionHex: '20000000',
+        merkleroot: 'd728487f6b559e46316605e2f4fdb7ff472017a5603c27523a3f41754aabfebf',
+        tx:
+         [ '887f1545a7fc2dd1af3cd2adacbe387d57ab1868a77b12d324c79f4446f95cb8',
+           '8660e931e59db9c37f3f1805e558e5dc2811fb90b5d36fadbcd489e6f0f1e058',
+           'ed26deabed91eca37a9df871855e0c29eed14c11ec6964bb4a25dfbfd274f9c7',
+           'a59dafb4e76bee152b25924545dbefc932179dcd03f24224dc6e4fc3922df120',
+           '3eed0b9964101e1f65796974a1eaf11cf654f2c6d8cde00a507d5b9bc0c519c6',
+           'ff10efc734c95a20a96b2dd62f4911b8dffe3c1bf06065c9b54d2d636156cddc',
+           '4df121a83914704ed6dd38434a05561de6c733e4ad0b19138c5747bea2d89632',
+           '48c66dc7cca2dd6cd54319db19e90dc9203a1a4cf7df7b1c36da1c333dc99a17',
+           '2a28269bb4b284b6fde5beb35feda500e4c164ccfe98ddd5611c52b9e5d5072e',
+           '95c2f3f6bbb68ec839de0fe66ed2c02a52dab345390b06f014ba66e8372ad654',
+           '96c5cb8e5060bc9d177e398acd529a12c7bd18085c5c72d7e20875d3f4a97a5a',
+           '0f4e99a3047905b47537a1e7d4c6db34bfb6dcb5c8ba8f52a77a7942283a5a6f',
+           '31938cc2db6fd1aa702cc461c53941dcb1895d80a958d689f40737244d67b29a',
+           '93a5ac08e4e026fc1ae40b54ed3534bb6d975806aefa84cf6ef67de3d1603be1' ],
+        time: 1519595851,
+        mediantime: 1519593171,
+        nonce: 1942317402,
+        bits: '18030655',
+        difficulty: 363501276434.3268,
+        chainwork: '0000000000000000000000000000000000000000008ac73e431fce0e00d0f62f',
+        previousblockhash: '000000000000000002187c90897b518c4da5122dcc42389a6f7461064f3ae7bd'
       }));
     });
 
@@ -415,23 +422,17 @@ class Server {
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify([
         {
-          "height" : 312647,
-          "hash" : "000000000b1be96f87b31485f62c1361193304a5ad78acf47f9164ea4773a843",
-          "branchlen" : 0,
-          "status" : "active"
+          "height": 518990,
+          "hash": "000000000000000002eafe2494f7dd1f678988217a12e8ab20d3594affd03392",
+          "branchlen": 0,
+          "status": "active"
         },
         {
-          "height" : 282072,
-          "hash" : "00000000712340a499b185080f94b28c365d8adb9fc95bca541ea5e708f31028",
-          "branchlen" : 5,
-          "status" : "valid-fork"
-        },
-        {
-          "height" : 281721,
-          "hash" : "000000006e1f2a32199629c6c1fbd37766f5ce7e8c42bab0c6e1ae42b88ffe12",
-          "branchlen" : 1,
-          "status" : "valid-headers"
-        },
+          "height": 518349,
+          "hash": "000000000000000001e349c5fbc653adcfd87c1a81c0ccc0e7443def968a54e6",
+          "branchlen": 1,
+          "status": "valid-headers"
+        }
       ]));
     });
 
@@ -457,21 +458,21 @@ class Server {
       res.setHeader('Content-Type', 'application/json');
 
       res.send({
-        "version" : 100000,
-        "protocolversion" : 70002,
-        "walletversion" : 60000,
-        "balance" : 1.27007770,
-        "blocks" : 315281,
-        "timeoffset" : 0,
-        "connections" : 9,
-        "proxy" : "",
-        "difficulty" : 1.00000000,
-        "testnet" : true,
-        "keypoololdest" : 1418924649,
-        "keypoolsize" : 101,
-        "paytxfee" : 0.00000000,
-        "relayfee" : 0.00001000,
-        "errors" : ""
+        version: 160200,
+        protocolversion: 70015,
+        walletversion: 130000,
+        balance: 0,
+        blocks: 518990,
+        timeoffset: 0,
+        connections: 14,
+        proxy: '',
+        difficulty: 363501276434.3268,
+        testnet: false,
+        keypoololdest: 1519061617,
+        keypoolsize: 100,
+        paytxfee: 0,
+        relayfee: 0.00001,
+        errors: ''
       });
     });
 
@@ -645,7 +646,7 @@ class Server {
     server.post('/getnetworkhashps', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send('79510076167');
+      res.send('2674221725221622000');
     });
 
     server.post('/getnetworkinfo', (req, res) => {
@@ -685,7 +686,7 @@ class Server {
         "relayfee": 5000.00000000,
         "localaddresses": [
           {
-            "address": "0600:3c03::f03c:91ff:fe89:dfc4",
+            "address": "0321:2c13::f03c:82ee:fe89:cec2",
             "port": 8333,
             "score": 4
           }
@@ -697,7 +698,7 @@ class Server {
     server.post('/getnewaddress', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send(BitcoinCash.fromWIF(store.get('addresses')[0].privateKeyWIF).getAddress());
+      res.send(BitcoinCash.toCashAddress(BitcoinCash.fromWIF(store.get('addresses')[0].privateKeyWIF).getAddress()));
     });
 
     server.post('/getpeerinfo', (req, res) => {
@@ -751,54 +752,74 @@ class Server {
     server.post('/getrawchangeaddress', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send('mnycUc8FRjJodfKhaj9QBZs2PwxxYoWqaK');
+      res.send(BitcoinCash.toCashAddress(BitcoinCash.fromWIF(store.get('addresses')[0].privateKeyWIF).getAddress()));
     });
 
     server.post('/getrawmempool', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
 
-      res.send(JSON.stringify({
-        "b104586f229e330caf42c475fd52684e9eb5e2d02f0fcd216d9554c5347b0873": {
-          "size": 485,
-          "fee": 0.00009700,
-          "modifiedfee": 0.00009700,
-          "time": 1479423635,
-          "height": 439431,
-          "startingpriority": 15327081.81818182,
-          "currentpriority": 21536936.36363636,
-          "descendantcount": 1,
-          "descendantsize": 485,
-          "descendantfees": 9700,
-          "ancestorcount": 1,
-          "ancestorsize": 485,
-          "ancestorfees": 9700,
-          "depends": [
-          ]
-        },
-        "094f7dcbc7494510d4daeceb2941ed73b1bd011bf527f6c3b7c897fee85c11d4": {
-          "size": 554,
-          "fee": 0.00005540,
-          "modifiedfee": 0.00005540,
-          "time": 1479423327,
-          "height": 439430,
-          "startingpriority": 85074.91071428571,
-          "currentpriority": 3497174.4375,
-          "descendantcount": 1,
-          "descendantsize": 554,
-          "descendantfees": 5540,
-          "ancestorcount": 1,
-          "ancestorsize": 554,
-          "ancestorfees": 5540,
-          "depends": [
-          ]
-        }
-      }));
+      res.send(JSON.stringify([
+      '6d23328804d12b8fc9a7cacf41eee9272ac5648ef209deefffacba2c31d13375',
+      '6a0377bf00d68cf8a8623f9b7f43eecd7c26a2057184d93f208dd4a31923efb6',
+      '5fc6fbde91aaeab8feb4fd3f8c9c1da7d157a5368e759ed4dd349924aefebae1',
+      'e079e017b2766adf8bea6bd7f5d50c09069cfc9f1424e5c40974d04561bd3e6d',
+      '003fd0ee7178899ec845e3aa534eef47077734d731ced213871e7eb5b00d8a69',
+      'd524d2ce8982d8af9127fd1285c2a2a3aca88b3a3c46bab04db2a9aacfc4add5',
+      '96d1dc2412478b27d98fff2f4b87d15fb376c2422cb077380c0a5203d5228793',
+      '1a54518db78cd3533653828b918da3f566f5512b8a039d6f22972f67f87cf844',
+      'e0b1ea4287c6a2829300a150d56c319bc1f24b4966d2405e613c778de6751c5f',
+      'f366b36c74361af7d5d0ba24b0f1355e104c16fd9a8b5f38be52418125aeea9a',
+      'b496b4b01d9a0880c1b5e6278e843c38799a4fd2bb34e7a62a1a56f68a7300b5',
+      'd40ff3e30231a825345e9530ba854e53cc052f8e20610f92f9d964cf00954b14',
+      'f86e3c20649cc374ece1aeaaf6e9e20cc361b4ad7236af7af930643edb45d708',
+      '1c93544c126f9814d81c1e68146e24b7b1e286c3a03cf2e25e30f60c483d6063',
+      '0176fcaa8cdd90f6ecad9b37597290bbb5b1465a9041d796700865a2eb0fc2a9',
+      '771326755f3a15a38947ccaf1142d8928df2d3cde88641c7fedd2fb8a85b90cc',
+      '99c1efc61a96293cc26d6db6d35a29af0dae216d630a697e58bf6ab78eb5b59f',
+      '5d2adf9d822e56e5c308bffb37ce07992e6943c1ac900f783ff290942915afda',
+      'f3ca3a4771aedb68249be75f6e004c509bebdb0d532108e1b8c745790c7589b8',
+      '03d863b378fb088847138d6b5f9d022b0f99a679931eb23dd176a322c0e295ae',
+      'b22a6b9ba8d4a5122db6b6c79abd16a751ff85fd2958303ba2cfebe9bac280ac',
+      '5c746c206b631a87e3920ea7dc3378a710176bcf995f6b7e423a64f577b95398',
+      '2ee43bc6061341550bb19e2e0c6a700ce21bfd438a37cd3f3acfed65870fca81',
+      '1e0a1c06881f16a02c630f57989a6a9571ed90843057220ce20dbba656f8be76',
+      'b532b4679627ef49c5c28de91c762b59f8031a0033eec828b1f7fd3c134d3766',
+      '4a6a42c2153fa4a6a70de7f09fdee766d9ceeb7a6dbe1a1495a17529f6364533',
+      '4e975f1d36ff8779f0b54713bac96d851002cca16f3dab2a0819fe2b78651b2b',
+      '004a6661ba3f31797e0f868d9465f63ab1e0041d15756467ba7eaccfe49b261a',
+      'de856283ed5f9c39a5a89f6fb051b7ab23f740331874bf88973ba2d734374feb',
+      '9490b1dfa731baa7a22348d676edd73bb89930defe6942fe032330a72c567dd8',
+      '1a567bed7bb50a69c9d7c66335c0cf97ea2553389f51c241fb2278ba46e4191d',
+      '32fb9dd3be9b14ab33cbee95b6581e08ac1456398d1978dd960ec18e06c317e6',
+      'a26486de58c5a177925f8deb0f321830a083e545c8b6804dd77178a116ba3f13',
+      '60448fbeaf1a828b211fb826872ba33101f83cbbb8224a5c358975950bb5ef9e',
+      '43a77e7c4ac422a4bccca48af0cf00b3a12de0f733688afe7e6761a5a0d3b484',
+      '87a282d846d2da4ba0bd94ff1026e04b90b0394cb104dafb402ef1b2ccba438f',
+      'aa64c0a1524c50df79795da399a558140374f664bc84f75729d43f10c349216a',
+      '435370286caac236dc9c932d14d85de97211ae6b26b1bdf955e58b28d2713729',
+      'f5d2846674b0ee4afc9c1332c883f5a7d7474a235ee7eccc0b9b8121aa4bbbf0',
+      '3738c0057952171a38b9a4346273b34e73815a9e8c6e1a47c8775711415684a4',
+      '5201e0f597c7b84763678045daad7a044c86a8ee9c6de0279d498c1cbebc9196',
+      '1d735e242e76b69aa6c1b4c906196323b76f98a9680325419864239081c277e5',
+      '623ac97e2581ee9defaa5bef1869b1361cce4124189d80592d9497811f3aa14c',
+      '4dfb66bb6736635d4390608cf42fb6c2be618ce4a82f00a34d339631ee9a85b8',
+      'fbad10410a13231de058aa5a713b65356463a03a4693eb91513205f98b91b8f1',
+      'ac6a49017af73aa237e35319e5fb2a2989673f5dae30d866abe6456f2b5858ed',
+      'cdb53985b723edcad2ad111b264ae7289f5c21462dba305e334144bd70eaa2e1',
+      'c280ab5f909c9483ab9999c4c72040c8822fe1f49b769dc69190a8e98316d3a8',
+      'c3a2c58ca679fa73805b000c12589d00e7b1fbe92ee15fb1cf90be352c8fd865',
+      '755c9f91eeb95df7ce6eab1ac3a48c0d13f5f6ac5c8d90d3b6d47c131ec73614',
+      'f0048a1cc7bf365651b5905b30c174d4380187b168dbb96ee24194f74a72130d',
+      '605a0cdd75a67caf6c8d06c855c7d48828d8e4c0f145d764d3bc5d85a26ad904',
+      '7854ee4e9984c195e1a5f512e621df4be79f7274d2784030c2d94e37f215eb19'
+      ]));
     });
 
     server.post('/getrawtransaction', (req, res) => {
       res.setHeader('Content-Type', 'application/json');
-
-      res.send(JSON.stringify({
+      let resp;
+      if(req.body.params[1] && req.body.params[1] == true) {
+        resp = {
           "hex": "0100000001bafe2175b9d7b3041ebac529056b393cf2997f7964485aa382ffa449ffdac02a000000008a473044022013d212c22f0b46bb33106d148493b9a9723adb2c3dd3a3ebe3a9c9e3b95d8cb00220461661710202fbab550f973068af45c294667fc4dc526627a7463eb23ab39e9b01410479be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8ffffffff01b0a86a00000000001976a91401b81d5fa1e55e069e3cc2db9c19e2e80358f30688ac00000000",
           "txid": "52309405287e737cf412fc42883d65a392ab950869fae80b2a5f1e33326aca46",
           "hash": "52309405287e737cf412fc42883d65a392ab950869fae80b2a5f1e33326aca46",
@@ -836,7 +857,13 @@ class Server {
           "confirmations": 374,
           "time": 1483591778,
           "blocktime": 1483591778
-      }));
+        };
+      } else {
+        resp = '0200000002b0e5d57d7ceb9329622792eb77d68971527a27dbd164f83c31f232b300b2ce40000000006a47304402205a6b937be935b7ef3c7507e65ac34f8466551c52be62c4eb0cc9c3232ed42b6202205ba4b7b76c05e5f2a10f74086257950df12ab2fc046e9d8e42304eeb791aff2041210338e9690b132d13a209dd5d549e21da1fee5fd8b391a9a183bb643f3fef6db110ffffffff681bc3b7293b887eeba19beaab30765ec2b16712d9df5c153d38988973cb05c9000000006b483045022100eb8136cbecccf125021c13fc953e6d669d41842f189a69f2060d7b4e5bbeba380220331642c3de3e1b016a0bfff0aa9c3a45b19078aa571ed10d90254137b87e6dd84121028a91bd260272d6e05845f602386ef744ecd8ffb15b68933e59c27ca1637bc911ffffffff02f8240100000000001976a914b0ef8fc1b127ac562ed8158cd11b7ed261b28bab88acd5be3e0f000000001976a914efb008ccec12d06248b33b38df962290673f8f3788ac00000000';
+
+      }
+
+      res.send(JSON.stringify(resp));
     });
 
     server.post('/getreceivedbyaccount', (req, res) => {
