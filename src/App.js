9f0de6d89990301fd053202d877e53b480037ef8
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-03-22 17:41:19 -0700

    Move code to Miner static methods.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,6 @@ import {
 } from 'react-router-dom';
 
 // custom models
-import Block from './models/Block';
-import Transaction from './models/Transaction';
-import Output from './models/Output';
-import Input from './models/Input';
 import underscore from 'underscore';
 
 import WalletContainer from './containers/WalletContainer'
@@ -32,7 +28,6 @@ import ExplorerContainer from './containers/ExplorerContainer'
 import Configuration from './components/Configuration';
 
 // utilities
-import BitcoinCash from './utilities/BitcoinCash';
 import Miner from './utilities/Miner';
 import reduxStore from './utilities/ReduxStore'
 
@@ -43,15 +38,6 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 // redux actions
-import {
-  updateWalletConfig
-} from './actions/ConfigurationActions';
-
-import {
-  createAccount,
-  updateAccount
-} from './actions/WalletActions';
-
 import {
   toggleVisibility
 } from './actions/ImportAndExportActions';
@@ -69,89 +55,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     // Set up default redux store
-    Miner.setUpStore()
-    this.createAccounts();
-  }
-
-  createAccounts() {
-    let walletConfig = reduxStore.getState().configuration.wallet;
-
-    if(walletConfig.autogenerateHDMnemonic) {
-      // create a random mnemonic w/ user provided entropy size
-      let randomBytes = bitbox.Crypto.randomBytes(walletConfig.entropy);
-      walletConfig.mnemonic = bitbox.Mnemonic.entropyToMnemonic(randomBytes, bitbox.Mnemonic.mnemonicWordLists()[walletConfig.language]);
-    }
-    let accounts = BitcoinCash.createAccounts(walletConfig);
-    reduxStore.dispatch(updateWalletConfig('mnemonic', walletConfig.mnemonic));
-    reduxStore.dispatch(updateWalletConfig('HDPath', walletConfig.HDPath));
-
-    accounts.forEach((account, index) => {
-      let xpriv = bitbox.HDNode.toXPriv(account);
-      let xpub = bitbox.HDNode.toXPub(account);
-
-      let formattedAccount = {
-        addresses: account.addresses,
-        title: '',
-        index: index,
-        privateKeyWIF:  bitbox.HDNode.toWIF(account),
-        xpriv: xpriv,
-        xpub: xpub
-      };
-      reduxStore.dispatch(createAccount(formattedAccount));
-    });
-
-    let blockchain = reduxStore.getState().blockchain;
-    let account1 = reduxStore.getState().wallet.accounts[0];
-    let account2 = reduxStore.getState().wallet.accounts[1];
-    let hex = Miner.createCoinbaseTx();
-
-    let mempool = reduxStore.getState().mempool;
-    bitbox.RawTransactions.decodeRawTransaction(mempool.transactions[0])
-    .then((result) => {
-      let inputs = [];
-      result.ins.forEach((vin, index) => {
-        inputs.push(new Input({
-          hex: vin.hex,
-          inputPubKey: vin.inputPubKey,
-          script: vin.script
-        }));
-      })
-
-      let outputs = [];
-      result.outs.forEach((vout, index) => {
-        outputs.push(new Output({
-          hex: vout.hex,
-          outputPubKey: vout.outputPubKey,
-          script: vout.script
-        }));
-      })
-
-      let tx = new Transaction({
-        value: 1250000000,
-        rawHex: hex,
-        timestamp: Date(),
-        hash: bitbox.Crypto.createSHA256Hash(hex),
-        inputs: inputs,
-        outputs: outputs
-      });
-
-      let blockData = {
-        index: 0,
-        transactions: [tx],
-        timestamp: Date()
-      };
-
-      let block = new Block(blockData)
-      let previousBlock = underscore.last(blockchain.chain) || {};
-      block.previousBlockHeader = previousBlock.header || "#BCHForEveryone";
-      block.header = bitbox.Crypto.createSHA256Hash(`${block.index}${block.previousBlockHeader}${JSON.stringify(block.transactions)}${block.timestamp}`);
-
-      reduxStore.dispatch(updateAccount(account1));
-      blockchain.chain.push(block);
-
-      Miner.mineBlock(blockchain)
-    }, (err) => { console.log(err);
-    });
+    Miner.setUpReduxStore()
+    Miner.createAccounts()
+    Miner.createCoinbaseTx();
   }
 
   handlePathMatch(path) {
--- src/utilities/Miner.js
+++ src/utilities/Miner.js
@@ -1,13 +1,16 @@
 import BitcoinCash from './BitcoinCash'
-import Bitcoin from 'bitcoinjs-lib';
-import Block from '../models/Block';
 import underscore from 'underscore';
 import reduxStore from './ReduxStore'
+import Block from '../models/Block';
+import Transaction from '../models/Transaction';
+import Output from '../models/Output';
+import Input from '../models/Input';
 
 import {
   createConfig,
   updateStore,
-  setExchangeRate
+  setExchangeRate,
+  updateWalletConfig
 } from '../actions/ConfigurationActions';
 
 import {
@@ -38,16 +41,17 @@ import {
 } from '../actions/ExplorerActions';
 
 import {
-  createWallet
+  createWallet,
+  createAccount,
+  updateAccount
 } from '../actions/WalletActions';
 
 import {
   createAccountSend
 } from '../actions/AccountSendActions';
 
-
 class Miner {
-  static setUpStore() {
+  static setUpReduxStore() {
     // Set up default redux store
     // configuration
     reduxStore.dispatch(createConfig());
@@ -80,6 +84,34 @@ class Miner {
     reduxStore.dispatch(createAccountSend());
   }
 
+  static createAccounts() {
+    let walletConfig = reduxStore.getState().configuration.wallet;
+
+    if(walletConfig.autogenerateHDMnemonic) {
+      // create a random mnemonic w/ user provided entropy size
+      let randomBytes = bitbox.Crypto.randomBytes(walletConfig.entropy);
+      walletConfig.mnemonic = bitbox.Mnemonic.entropyToMnemonic(randomBytes, bitbox.Mnemonic.mnemonicWordLists()[walletConfig.language]);
+    }
+    let accounts = BitcoinCash.createAccounts(walletConfig);
+    reduxStore.dispatch(updateWalletConfig('mnemonic', walletConfig.mnemonic));
+    reduxStore.dispatch(updateWalletConfig('HDPath', walletConfig.HDPath));
+
+    accounts.forEach((account, index) => {
+      let xpriv = bitbox.HDNode.toXPriv(account);
+      let xpub = bitbox.HDNode.toXPub(account);
+
+      let formattedAccount = {
+        addresses: account.addresses,
+        title: '',
+        index: index,
+        privateKeyWIF:  bitbox.HDNode.toWIF(account),
+        xpriv: xpriv,
+        xpub: xpub
+      };
+      reduxStore.dispatch(createAccount(formattedAccount));
+    });
+  }
+
   static mineBlock(blockchain) {
     reduxStore.dispatch(addBlock(blockchain));
 
@@ -91,6 +123,7 @@ class Miner {
   }
 
   static createCoinbaseTx() {
+    let blockchain = reduxStore.getState().blockchain;
     let account1 = reduxStore.getState().wallet.accounts[0];
     let account2 = reduxStore.getState().wallet.accounts[1];
     let alice = bitbox.HDNode.fromWIF(account1.privateKeyWIF);
@@ -106,7 +139,54 @@ class Miner {
 
     // bump address counter to next address
     account1.addresses.nextChainAddress(0);
-    return hex;
+
+    let mempool = reduxStore.getState().mempool;
+    bitbox.RawTransactions.decodeRawTransaction(mempool.transactions[0])
+    .then((result) => {
+      let inputs = [];
+      result.ins.forEach((vin, index) => {
+        inputs.push(new Input({
+          hex: vin.hex,
+          inputPubKey: vin.inputPubKey,
+          script: vin.script
+        }));
+      })
+
+      let outputs = [];
+      result.outs.forEach((vout, index) => {
+        outputs.push(new Output({
+          hex: vout.hex,
+          outputPubKey: vout.outputPubKey,
+          script: vout.script
+        }));
+      })
+
+      let tx = new Transaction({
+        value: 1250000000,
+        rawHex: hex,
+        timestamp: Date(),
+        hash: bitbox.Crypto.createSHA256Hash(hex),
+        inputs: inputs,
+        outputs: outputs
+      });
+
+      let blockData = {
+        index: 0,
+        transactions: [tx],
+        timestamp: Date()
+      };
+
+      let block = new Block(blockData)
+      let previousBlock = underscore.last(blockchain.chain) || {};
+      block.previousBlockHeader = previousBlock.header || "#BCHForEveryone";
+      block.header = bitbox.Crypto.createSHA256Hash(`${block.index}${block.previousBlockHeader}${JSON.stringify(block.transactions)}${block.timestamp}`);
+
+      reduxStore.dispatch(updateAccount(account1));
+      blockchain.chain.push(block);
+
+      Miner.mineBlock(blockchain)
+    }, (err) => { console.log(err);
+    });
   }
 }
 
