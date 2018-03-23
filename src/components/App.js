84a19d91f73409f7e30288296839347cc74b0e45
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-03-23 07:38:58 -0700

    theme scss.

--- src/components/App.js
+++ src/components/App.js
@@ -32,6 +32,7 @@ import Miner from '../utilities/Miner';
 import reduxStore from '../utilities/ReduxStore'
 
 // css
+import '../styles/theme.scss';
 import '../styles/app.scss';
 
 import { Provider } from 'react-redux'
